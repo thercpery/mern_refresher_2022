d207c7b1db30b783face32ba1e8ad477d8c6ed99
Roy Christian Pery <thercpery@gmail.com>
2022-07-18 11:44:19 +0800

    added jwt authorizations

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 app.use("/api/goals", require("./routes/goals"))
+app.use("/api/users", require("./routes/users"))
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
